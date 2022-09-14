067fdc6b0cbe032a7daad826187e4a752c03fcfe
aqsafaisal34 <aqsay.faisal34@gmail.com>
2022-09-14 17:26:10 +0500

    first commit

---
+++ src/USComponent.js
@@ -0,0 +1,50 @@
+// Destructuring Method(correct)
+import React from 'react'
+import { useState } from 'react'
+
+export default function USComponent() {
+    const [name,setName] = useState("Ali")
+  return (
+    <div>
+      <h1>{name}</h1>
+      <button onClick={()=> setName("Laiba")}>Update</button>
+    </div>
+  )
+}
+
+
+
+
+
+
+// update by creating function
+// import React from 'react'
+// import { useState } from 'react'
+
+// export default function USComponent() {
+//     const myArray = useState("Aqsa")
+//     let changeState = () =>{
+//         myArray[1]("Laiba")
+//     }
+//   return (
+//     <div>
+//       <h1>{myArray[0]}</h1>
+//       <button onClick={changeState}>Update</button>
+//     </div>
+//   )
+// }
+
+
+// store and update with Array
+// import React from 'react'
+// import { useState } from 'react'
+
+// export default function USComponent() {
+//     const myArray = useState("Aqsa")
+//   return (
+//     <div>
+//       <h1>{myArray[0]}</h1>
+//       <button onClick={()=> myArray[1]("Laiba")}>Update</button>
+//     </div>
+//   )
+// }
